97c80035f7665a8be96d55529d39159cf75dd2a9
jlcorrei <103605432+jlcorrei@users.noreply.github.com>
2025-10-29 03:34:02 -0400

    multi-database creation

--- proj2/backend/app/app.py
+++ proj2/backend/app/app.py
@@ -3,10 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def get_app():
+def get_app(config_name):
     app = Flask(__name__)
-    password = ""
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:{password}@localhost/movie_munchers'
+    user = 'root'
+    password = ''
+    host = 'localhost'
+
+    if config_name == 'production':
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{user}:{password}@{host}/movie_munchers_prod'
+    elif config_name == 'development':
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{user}:{password}@{host}/movie_munchers_dev'
+    elif config_name == 'testing':
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{user}:{password}@{host}/movie_munchers_test'
+        app.config['TESTING'] = True
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     return app
